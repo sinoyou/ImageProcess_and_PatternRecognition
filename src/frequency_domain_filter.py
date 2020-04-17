a1b3db108c5db12a960b6ca1e2488d67e82ccb16
sinoyou <youzn99@qq.com>
2020-04-17 18:25:06 +0800

    Add ideal LPF and butter worth LPF.

--- src/frequency_domain_filter.py
+++ src/frequency_domain_filter.py
@@ -1,6 +1,50 @@
 import numpy as np
+from src.utils import mirror
 
 
-def ideal_low_pass_filter(fre_domain):
+def ideal_low_pass_filter(fre_domain, **kwargs):
+    """
+    理想低通滤波器
+    允许指定的参数：
+     - D_0(u, v)
+    """
+    if 'clip_d' not in kwargs.keys():
+        print('Clip D set to 50 by default.')
+        clip_d = 50
+    else:
+        clip_d = kwargs['clip_d']
+
     height, width = fre_domain.shape
-   
+    center = height // 2, width // 2
+    mirror_fre_domain = mirror(fre_domain)
+    mask = np.zeros_like(fre_domain)
+    for h in range(height):
+        for w in range(width):
+            if (h - center[0]) ** 2 + (w - center[1]) ** 2 <= clip_d ** 2:
+                mask[h][w] = 1.0
+    result = mirror(np.multiply(mask, mirror_fre_domain))
+    return result
+
+
+def butterworth_low_pass_filter(fre_domain, **kwargs):
+    """
+    ButterWorth 低通滤波器
+    允许指定的参数
+     - 截断频率 D_0(u, v)
+    """
+    if 'clip_d' not in kwargs.keys():
+        print('Clip D set to 50 by default.')
+        clip_d = 50
+    else:
+        clip_d = kwargs['clip_d']
+
+    height, width = fre_domain.shape
+    center = height // 2, width // 2
+    mirror_fre_domain = mirror(fre_domain)
+    mask = np.zeros_like(fre_domain)
+    for h in range(height):
+        for w in range(width):
+            d = (h - center[0]) ** 2 + (w - center[1]) ** 2
+            mask[h][w] = 1.0 / (1.0 + d / (clip_d ** 2))
+    result = mirror(np.multiply(mask, mirror_fre_domain))
+    return result
